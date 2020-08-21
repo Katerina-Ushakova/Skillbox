d66e0ff57a8f36c3f220c6d7f7ea45701ded7dd7
Katerina Ushakova <66923824+Katerina-Ushakova@users.noreply.github.com>
2020-08-21 23:45:42 +0300

    /

--- 06_InheritanceAndPolymorphism/Simple Chatty Bot/Problems/Check prefix ignoring the case/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        System.out.println(text.toUpperCase().startsWith("J"));
-    }
-}
--- 06_InheritanceAndPolymorphism/Simple Chatty Bot/Problems/Invalid declaration of variables/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        long a = 512343;
-        long b = 3431231;
-        System.out.println(a + b);
-    }
-}
